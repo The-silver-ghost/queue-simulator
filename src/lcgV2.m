78d9da13bdd074c4274254fb7fe74d5a601fefa0
kashvin08 <kashvin116300@gmail.com>
2025-06-08 12:59:34 +0800

    Update lcgV2.m

--- src/lcgV2.m
+++ src/lcgV2.m
@@ -1,15 +1,21 @@
-m = 1000;   
-a = 21;      
-c = 43;    
-x0 = 12;   
-n = 10;
+function random_numbers = lcgV3(n, a, c, m, x0)
 
-random_numbers = zeros(1, n);
+    %nargin command (number of input arguments) is used to check how many arguments are passed.  
+    
+    if nargin < 5, x0 = 12; end
+    if nargin < 4, m = 1000; end
+    if nargin < 3, c = 43; end
+    if nargin < 2, a = 21; end
+    
+    %default numbers are assigned if no input is given.
 
-for i = 1:n
-    x0 = mod(a * x0 + c, m);
-    random_numbers(i) = x0;
+    random_numbers = zeros(1, n); 
+
+    for i = 1:n
+        x0 = mod(a * x0 + c, m);
+        random_numbers(i) = x0;
+    end
+
+    disp('the generated numbers are:');
+    disp(random_numbers);
 end
-
-disp('random numbers are:');
-disp(random_numbers);
