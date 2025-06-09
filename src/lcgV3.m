0d2899a64c4a4e47967a1df9ad32b5f8d470c118
kashvin08 <kashvin116300@gmail.com>
2025-06-09 20:49:05 +0800

    Update lcgV4.m

--- src/lcgV3.m
+++ src/lcgV3.m
@@ -1,21 +1,25 @@
-function random_numbers = lcgV3(n, a, c, m, x0)
+function lcgV4(a, c, m, x0)
 
-    %nargin command (number of input arguments) is used to check how many arguments are passed.  
-    
-    if nargin < 5, x0 = 12; end
-    if nargin < 4, m = 1000; end
-    if nargin < 3, c = 43; end
-    if nargin < 2, a = 21; end
-    
-    %default numbers are assigned if no input is given.
-
-    random_numbers = zeros(1, n); 
-
-    for i = 1:n
-        x0 = mod(a * x0 + c, m);
-        random_numbers(i) = x0;
+    if nargin < 4
+        x0 = mod(round(sum(clock) * 100), m);  % Use system time to randomize seed
     end
 
-    disp('the generated numbers are:');
-    disp(random_numbers);
+    b = 1;
+    n = 1;
+    values = [];
+    total = 0;
+
+    while b > 0.01 
+        x0 = mod(a * x0 + c, m);
+        val = round((x0 / m) * 100) / 100; 
+        if val > b
+            val = b; 
+        end
+        values(end+1) = val;
+        total = total + val;
+        b = 1 - total;
+    end
+
+    fprintf('Generated values:\n');
+    disp(values);
 end
