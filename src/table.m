8ca5cf099be02e1f7088482bcf301c1f85a798f6
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-06-17 10:01:07 +0800

    made table function to reduce repetition

--- src/table.m
+++ src/table.m
@@ -4,13 +4,19 @@ function [col_time,col_prob,col_cdf] = table(prob_array)
   numberOfValues = 0;
   beginValue = randi([1,3]);
   col_prob = prob_array;
+  tempNum = 0;
+  %cdf = [];
 
   %find number of values generated
   numOfValues = length(prob_array);
 
-  % generate refuel times
   for i = 1:numOfValues
+    % generate refuel times
     col_time(i) = beginValue;
     beginValue += 1;
+
+    % calculate cdf
+    tempNum += prob_array(i);
+    col_cdf(i) = tempNum;
   endfor
 end
