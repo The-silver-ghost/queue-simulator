1eef4d1460c69bfdf54b51ab69d90ce0c6c76a68
kashvin08 <kashvin116300@gmail.com>
2025-06-22 22:07:36 +0800

    Update lcgV4.m

--- src/lcgV4.m
+++ src/lcgV4.m
@@ -1,22 +1,11 @@
-function values = lcgV4(a, c, x0)
-m = 3 + (7-3)*(rand());
-    if nargin < 4
-        x0 = mod(round(sum(clock) * 100), m);  %time system to randomize
-    end
-
-    b = 1;
-    n = 1;
-    values = [];
-    total = 0;
-
-    while b > 0.01
-        x0 = mod(a * x0 + c, m);
-        val = round((x0 / m) * 100) / 100;
-        if val > b
-            val = b;
-        end
-        values(end+1) = val;
-        total = total + val;
-        b = 1 - total;
-    end
+function values = lcgV4(numOfVehicles)
+initial = randi([1, 100]); %generates random initial number between 1 and 100;
+a = 21; % value a is set to default as 21;
+c = 43; % value c is set to default as 43;
+x0 = 100; % value x0 is set to default as 100;
+x = mod((a * initial + c), x0);  % lcg formula is used to generate first number;
+for i = 1:numOfVehicles % for loop is used to iterate from 1 to the number of vehicles present at the gas station;
+    x = mod((a * x + c), x0); % the same lcg formula is used to generate new number for each iteration and stores in an array;
+    values(i) = x; % after iteration is completed, function returns array which contains the random values; 
+end
 end
